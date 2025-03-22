bd1031431c95b81de6c3e90c9126652a2c161449
Carl <cmillardv77@gmail.com>
2025-03-22 12:38:20 -0400

    Dev Branch

--- forms.py
+++ forms.py
@@ -1,21 +1,25 @@
+import datetime
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, ColorField, StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 
+now = datetime.datetime.now()
+formatted_date= now.strftime("%m/%d/%y")
+
 # For Registering a new user
 class NewUserForm(FlaskForm):
     email = StringField("E-mail Address", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired])
+    password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Sign up")
 # For registered users to log in
 class LoginForm(FlaskForm):
     email = StringField("E-mail Address", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired])
+    password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Log in")
 
 # To generate the name of each to-do list, which will be the parent to items in the to-do items table
 class ToDoNameForm(FlaskForm):
-    to_do_name = StringField("List Name")
+    to_do_name = StringField("", default=f"My List - {formatted_date}", validators=[DataRequired()])
     # Placeholder below - want to let user click list name, press "Enter" to change
     # submit  = SubmitField("Enter Name")
     # date will be automatically applied via first submission
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from datetime import date
-from flask import Flask, render_template
+from flask import Flask, flash, redirect,render_template, request, url_for
 from flask_bootstrap import Bootstrap5
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
+from forms import LoginForm, NewUserForm, ToDoNameForm
 import os
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Text, ForeignKey
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, Table
+from typing import List
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -16,26 +18,48 @@ login_manager = LoginManager()
 class Base(DeclarativeBase):
     pass
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///to-dos.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
+login_manager.init_app(app)
 Bootstrap5(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(Users, user_id)
 
-class User(db.Model):
+relationships_table = Table(
+    "relationships_table",
+    Base.metadata,
+    Column("user_id", ForeignKey("users.id"), primary_key=True),
+    Column("list_name_id", ForeignKey("list_names.id"), primary_key=True),
+)
+
+class Users(db.Model, UserMixin):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(250), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(250), nullable=False)
+    list_names: Mapped[List['ListName']] = relationship(secondary=relationships_table, back_populates="list_users")
 
 class ListName(db.Model):
-    __tablename__ = "list_name"
+    __tablename__ = "list_names"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    list_name: Mapped[str] = mapped_column(String(250), nullable=False)
+    list_users: Mapped[List['Users']] = relationship(secondary=relationships_table, back_populates="list_names")
+    list_items: Mapped[List['ToDoItem']] = relationship()
+    list_url: Mapped[str] = mapped_column(String(), nullable=False, unique=True)
 
 class ToDoItem(db.Model):
     __tablename__ = "to_do_items"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    item: Mapped[str] = mapped_column(String(250), nullable=False)
+    list_name = relationship('ListName', back_populates='list_items')
+    list_name_id: Mapped[int] = mapped_column(ForeignKey('list_names.id'))
+    order_num: Mapped[int] = mapped_column(Integer, nullable=False)
 
-class Relationship(db.Model):
-    __tablename__ = "relationship_table"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def homepage():
@@ -43,19 +67,61 @@ def homepage():
 
 @app.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
-    return render_template("signup.html")
+    form = NewUserForm()
+    if form.validate_on_submit():
+        result = db.session.execute(db.select(Users).where(Users.email == form.email.data))
+        user = result.scalar()
+        if user:
+            flash("An account using that email already exists.")
+            return redirect(url_for('log_in'))
+        hs_password = generate_password_hash(password=request.form.get('password'), method='pbkdf2:sha256', salt_length=8)
+
+        new_user = Users(
+            email = request.form.get('email'),
+            password = hs_password)
+
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        print('registered and logging in!')
+        return redirect(url_for('all_lists'))
+    return render_template("signup.html", form=form)
 
 @app.route("/log-in", methods=["GET", "POST"])
 def log_in():
-    return render_template("login.html")
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = request.form.get('email')
+        password = request.form.get('password')
+        result = db.session.execute(db.select(Users).where(Users.email == email))
+        user = result.scalar()
+        if not user:
+            flash("No account exists under the provided email address.")
+        elif not check_password_hash(user.password, password):
+            flash("The password entered is invalid.")
+        else:
+            login_user(user)
+            print('logging in!')
+            return redirect(url_for('all_lists'))
+    return render_template("login.html", form=form)
 
-@app.route("/all-lists")
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('log_in'))
+
+@app.route("/all")
 def all_lists():
-    pass
+    return render_template("lists.html", current_user=current_user)
 
 @app.route("/list", methods=["GET", "POST"])
 def current_list():
     pass
+
+@app.route("/new-list", methods=["GET", "POST"])
+def new_list():
+    form = ToDoNameForm()
+    return render_template('new-list.html', current_user=current_user, form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
